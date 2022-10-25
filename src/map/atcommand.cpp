f6ae05d15dbb4a0023f2b9673c8c490fde2abf96
kgb1990 <ragnarok.ep1990@gmail.com>
2022-10-26 04:22:01 +0700

    command afk

--- src/map/atcommand.cpp
+++ src/map/atcommand.cpp
@@ -1343,7 +1343,7 @@ ACMD_FUNC(heal)
 
 /*
 * apache
- * ‡∏ü‡∏±‡∏á‡∏ä‡∏±‡πà‡∏ô‡∏Å‡∏¥‡∏à‡∏Å‡∏£‡∏£‡∏°‡∏ó‡∏≠‡∏¢‡πÄ‡∏ï‡πã‡∏≤
+ * ø—ß™—Ëπ°‘®°√√¡∑Õ¬‡µÎ“
 */
 ACMD_FUNC(dice)
 	{
@@ -10834,6 +10834,45 @@ ACMD_FUNC(addfame)
 	return 0;
 }
 
+/*==========================================
+* @afk
+*------------------------------------------*/
+ACMD_FUNC(afk) {
+ 
+        nullpo_retr(-1, sd);
+				
+				if( map_getcell(sd->bl.m,sd->bl.x,sd->bl.y,CELL_CHKNOVENDING) ) {
+					clif_displaymessage(fd, "‰¡Ë “¡“√∂„™È §” —Ëß @afk „°≈È NPC ‰¥È");
+				return true;
+				}
+				
+				if( pc_isdead(sd) ) {
+					clif_displaymessage(fd, "‰¡Ë “¡“√∂„™È§” —Ëß @afk ∂È“§ÿ≥µ“¬.");
+				return -1;
+				}
+				
+                if( battle_config.autotrade_mapflag == map_getmapflag(sd->bl.m, MF_AUTOTRADE))
+                {
+
+                if(map_getmapflag(sd->bl.m, MF_PVP)  || map_getmapflag(sd->bl.m, MF_GVG)){
+					clif_displaymessage(fd, "‰¡Ë “¡“√∂„™È§” —Ëß@afk „π·¡æ PVP À√◊Õ GVG.");
+                return -1;}
+
+                        sd->state.autotrade = 1;
+			 			pc_setsit(sd);
+                        skill_sit(sd,1);
+                        clif_sitting(&sd->bl);                     
+                        if( battle_config.afk_timeout )
+                        {
+                                int timeout = atoi(message);
+                                status_change_start(NULL, &sd->bl, SC_AUTOTRADE, 10000,0,0,0,0, ((timeout > 0) ? min(timeout,battle_config.afk_timeout) : battle_config.afk_timeout)*60000,0);
+                        }
+                        clif_authfail_fd(fd, 15);
+                } else
+					clif_displaymessage(fd, "‰¡Ë “¡“√∂„™È §” —Ëß @afk „π·¡æπ’È‰¥È.");
+        return 0;
+}
+
 /**
  * Opens the enchantgrade UI
  * Usage: @enchantgradeui
@@ -11152,6 +11191,7 @@ void atcommand_basecommands(void) {
 		ACMD_DEF(fontcolor),
 		ACMD_DEF(langtype),
 		ACMD_DEF(dice), // apache
+		ACMD_DEF(afk),  // apache
 #ifdef VIP_ENABLE
 		ACMD_DEF(vip),
 		ACMD_DEF(showrate),
